127db7f7258d735fb3883936d17eba680eba8527
João Antônnio Martinez <133376282+JamizM@users.noreply.github.com>
2024-09-04 12:45:49 -0300

    alteracoes

--- Exercicio/Contador.java
+++ Exercicio/Contador.java
@@ -1,3 +1,4 @@
+package Exercicio;
 import java.util.Scanner;
 
 public class Contador {
--- Exercicio/ParametrosInvalidosException.java
+++ Exercicio/ParametrosInvalidosException.java
@@ -1,3 +1,4 @@
+package Exercicio;
 public class ParametrosInvalidosException extends Exception{
     
     public ParametrosInvalidosException(){
